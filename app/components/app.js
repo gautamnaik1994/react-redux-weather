382d469013c3e6910f2050484c13e2ab85682f65
Gautam Naik <gautamnaik1994@gmail.com>
2017-03-11 04:28:25 +0530

    fc

--- app/components/app.js
+++ app/components/app.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import '../styles/app.scss';
 
 import SearchBar from 'search_bar';
+import WeatherList from 'weather_list';
 class App extends Component {
     render() {
         return (
             <div className="container">
-                <SearchBar/>
+                <SearchBar />
+                <WeatherList/>
             </div>
         );
     }
