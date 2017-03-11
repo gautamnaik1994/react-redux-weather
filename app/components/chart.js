4a9062e3f6e560ffcfc9794b74ef9f76e50bf0bb
gautamnaik1994 <gautamnaik1994@gmail.com>
2017-03-11 16:31:58 +0530

    fc

---
+++ app/components/chart.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Sparklines, SparklinesLine,SparklinesReferenceLine } from 'react-sparklines';
+import _ from 'lodash';
+
+function average(data) {
+    // var sum = data.reduce((acc, val) => { return acc + val; }, 0);
+    // return sum / data.length;
+   return  (_.sum(data)/data.length)
+}
+
+export default (props) => {
+    return (
+        <div>
+            <Sparklines height={50} width={100} data={props.data}>
+                <SparklinesLine color={props.color} />
+                <SparklinesReferenceLine type="avg"/>
+            </Sparklines>    
+            <div>{average(props.data)}</div>
+        </div>  
+    );
+}
--- app/containers/weather_list.js
+++ app/containers/weather_list.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import Chart from 'chart';
 
 class WeatherList extends Component {
     renderWeather(cityData) {
         const name = cityData.city.name;
-        const temps = cityData.list.map(weather => weather.main.temp-273.15);
+        const temps = cityData.list.map(weather => weather.main.temp - 273.15);
+        const pressures = cityData.list.map(weather => weather.main.pressure);
+        const humidities = cityData.list.map(weather => weather.main.humidity);
         console.log(temps);
         return (
             <tr key={name}>
                 <td>{name}</td>
-                <td>
-                    <Sparklines height={50} width={180} data={temps}>
-                        <SparklinesLine color="lawngreen"/>    
-                    </Sparklines>    
-                </td>
+                <td><Chart data={temps} color="green" /></td>
+                <td><Chart data={pressures} color="red" /></td>
+                <td><Chart data={humidities} color="blue"/></td>
             </tr>
         )
     }
