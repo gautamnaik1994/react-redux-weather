06e00c8bf0ee94ac8c342743e000464c6dde3079
Gautam Naik <gautamnaik1994@gmail.com>
2017-03-13 23:55:26 +0530

    fc

--- app/index.js
+++ app/index.js
@@ -2,13 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware,compose } from 'redux';
 import ReduxPromise from 'redux-promise';
 import App from './components/app';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
-const store = createStoreWithMiddleware(reducers);
+// const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+// const store = createStoreWithMiddleware(reducers);
+
+var configure = (initialState = {}) => {
+  
+  var store = createStore(reducers, initialState, compose(
+    applyMiddleware(ReduxPromise),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  ));
+  if (module.hot) {
+    module.hot.accept('./reducers', () => {
+      const nextRootReducer = require('./reducers/index');
+      store.replaceReducer(nextRootReducer);
+    });
+  }
+  return store;
+};
 
 
 // const render = (Component) => {
@@ -26,7 +41,7 @@ const store = createStoreWithMiddleware(reducers);
 const render = (Component) => {
   ReactDOM.render(
     <AppContainer>
-      <Provider store={store}>
+      <Provider store={configure()}>
         <Component />
       </Provider>
     </AppContainer>,
